8c53ac16b132295cdd88cf3c69efeb8158fd2532
Brian Ji <bji@usc.edu>
2017-09-07 17:33:58 -0700

    Added webserver control, LCD readout, switches

--- wemos/wemos.ino
+++ wemos/wemos.ino
@@ -6,31 +6,69 @@
 #include "UpnpBroadcastResponder.h"
 #include "CallbackFunction.h"
 
+//for web service
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+
+MDNSResponder mdns;
+String webPage = "";
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+// pin declarations for SPI LCD
+#define OLED_MOSI  13   //D1
+#define OLED_CLK   15   //D0
+#define OLED_DC    14
+#define OLED_CS    2
+#define OLED_RESET 12
+Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
+#define SWITCH 16
+int switchState;
+
 // prototypes
 boolean connectWifi();
 
 //on/off callbacks 
-void officeLightsOn();
-void officeLightsOff();
-void kitchenLightsOn();
-void kitchenLightsOff();
+void lightsOn();
+void lightsOff();
+void fanOn();
+void fanOff();
 
 // Change this before you flash
-const char* ssid = "Aruna";
-const char* password = "*****";
+const char* ssid = "Lorenzo_2.4";
+const char* password = "";
+
+//used for real time
+String timeDate = "";
+
+//used for time tracking
+unsigned long previousMillis = 0;  
+const long interval = 7000; //wait 3 seconds before next update
+bool menuChange = 0;
 
 boolean wifiConnected = false;
 
 UpnpBroadcastResponder upnpBroadcastResponder;
 
-Switch *office = NULL;
-Switch *kitchen = NULL;
+//NOTICE: declare alexa callwords here
+String switchName = "lights";
+String switchName2 = "fan";
+Switch *lights = NULL;  //pin D3, GPIO 
+Switch *fan = NULL;   //pin D2, GPIO 
 
 void setup()
 {
+  delay(150);
   Serial.begin(9600);
+  pinMode(0, OUTPUT); //relay 
+pinMode(4, OUTPUT); //relay pin
+  //pinMode(14, OUTPUT); //relay pin
+  pinMode(SWITCH, INPUT_PULLUP); //switch pin
    
-  // Initialise wifi connection
+  // Initialize wifi connection
   wifiConnected = connectWifi();
   
   if(wifiConnected){
@@ -38,39 +76,120 @@ void setup()
     
     // Define your switches here. Max 14
     // Format: Alexa invocation name, local port no, on callback, off callback
-    office = new Switch("office lights", 80, officeLightsOn, officeLightsOff);
-    kitchen = new Switch("kitchen lights", 81, kitchenLightsOn, kitchenLightsOff);
+    lights = new Switch(switchName, 80, lightsOn, lightsOff);
+    fan = new Switch(switchName2, 81, fanOn, fanOff);
 
     Serial.println("Adding switches upnp broadcast responder");
-    upnpBroadcastResponder.addDevice(*office);
-    upnpBroadcastResponder.addDevice(*kitchen);
+    upnpBroadcastResponder.addDevice(*lights);
+    upnpBroadcastResponder.addDevice(*fan);
   }
+
+  //initialize display
+  display.begin(SSD1306_SWITCHCAPVCC);
+  display.display();
+  delay(1000);
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+
+  //init web server
+
+  if (mdns.begin("esp8266", WiFi.localIP())) {
+    Serial.println("MDNS responder started");
+  }
+
+  //server.begin();
+  Serial.println("All Services Started");
 }
  
 void loop()
 {
-	 if(wifiConnected){
-      upnpBroadcastResponder.serverLoop();
+  unsigned long currentMillis = millis();
+  unsigned long delta = currentMillis - previousMillis;
+  if (delta >= interval) {
+    previousMillis = currentMillis - 1000;
+    if (menuChange == LOW) {
+      menuChange = HIGH;
+    } else {
+      menuChange = LOW;
+    }
+
+  }
+      //printStatus(wifiConnected);
+      switchState = digitalRead(SWITCH);
+      bool lightState = digitalRead(15);
       
-      kitchen->serverLoop();
-      office->serverLoop();
+	 if(wifiConnected){
+      if (menuChange) {
+      printAltStatus();
+      }
+      else {
+      printStatus(wifiConnected);
+      }
+      upnpBroadcastResponder.serverLoop();
+      fan->serverLoop();
+      lights->serverLoop();
+
 	 }
+
+    //ext toggle switch
+   if (switchState == 0) {
+    delay(20); //debouncing
+    if (lightState) {
+      digitalWrite(0, LOW);
+    }
+    else {
+      digitalWrite(0, HIGH);
+    }
+    delay(2000); //prevent strobe
+   }
 }
 
-void officeLightsOn() {
-    Serial.print("Switch 1 turn on ...");
+void lightsOn() {
+    Serial.print("Lights turn on ...");
+    digitalWrite(0, HIGH);
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(25,25);
+  display.println("Lights On");
+  display.display();
+  delay(1000);
+
 }
 
-void officeLightsOff() {
-    Serial.print("Switch 1 turn off ...");
+void lightsOff() {
+    Serial.print("Lights turn off ...");
+    digitalWrite(0, LOW);
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(25,25);
+  display.println("Lights Off");
+  display.display();
+  delay(1000);
 }
 
-void kitchenLightsOn() {
-    Serial.print("Switch 2 turn on ...");
+void fanOn() {
+  Serial.print("Fan turn on ...");
+  digitalWrite(4, HIGH);
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(25,25);
+  display.println("Fan On");
+  display.display();
+  delay(1000);
 }
 
-void kitchenLightsOff() {
-  Serial.print("Switch 2 turn off ...");
+void fanOff() {
+  Serial.print("Fan turn off ...");
+  digitalWrite(4, LOW);
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(25,25);
+  display.println("Fan Off");
+  display.display();
+  delay(1000);
 }
 
 // connect to wifi – returns true if successful or false if not
@@ -108,4 +227,71 @@ boolean connectWifi(){
   }
   
   return state;
+}
+
+void printVal(boolean value)
+{
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(25,10);
+  if (value == 1) {
+  display.println("READY");
+  }
+  else {
+    display.println("ERROR");
+  }
+}
+
+void printTitle()
+{
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(10,0);
+  display.println("IP: ");
+  display.setCursor(30,0);
+  display.println(WiFi.localIP());
+}
+void printTemp(float value)
+{
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(10,40);
+  display.println("Temperature: ");
+  display.setCursor(60,40);
+  display.println(value);
+}
+
+void printStatus(boolean value)
+{
+      printTitle();
+      printVal(value);
+      //printTemp(value);
+      display.display();
+}
+
+void printAltStatus()
+{
+//title
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.println("SSID: ");
+  display.setCursor(35,0);
+  display.println(ssid);
+
+//body
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,10);
+  display.println("1:");
+  display.setCursor(20,10);
+  display.println(lights->getAlexaInvokeName());
+  display.setCursor(0,20);
+  display.println("2:");
+  display.setCursor(20,20);
+  display.println(fan->getAlexaInvokeName());
+//display
+  display.display();
 }
